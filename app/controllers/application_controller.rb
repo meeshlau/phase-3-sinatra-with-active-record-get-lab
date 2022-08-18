3c84868e56b10cc29caf4a9384b869b4fc15edbd
Michelle Lau <meeshklau@gmail.com>
2022-08-18 11:53:19 -1000

    commit tree bbdf38c7e3667f54e1d9ce0a65cfdc21c2b24cce

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,26 @@
 class ApplicationController < Sinatra::Base
 
+  set :default_content_type, 'application/json'
+
   # add routes
+  get '/bakeries' do
+    bakeries = Bakery.all
+    bakeries.to_json
+  end
+
+  get '/bakeries/:id' do
+    bakeries = Bakery.find(params[:id])
+    bakeries.to_json(include: :baked_goods)
+  end
+
+  get '/baked_goods/by_price' do
+    baked_goods_by_price = BakedGood.order(price: :desc)
+    baked_goods_by_price.to_json
+  end
+
+  get '/baked_goods/most_expensive' do
+    most_expensive = BakedGood.order(price: :desc)
+    most_expensive[0].to_json(only: [:name, :price])
+  end
 
 end
